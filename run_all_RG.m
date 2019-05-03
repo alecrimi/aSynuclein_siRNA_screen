57cc549f05f95c3a967fcd1ee460afae0f9634b8
Alessandro Crimi <alessandro.crimi@iit.it>
2019-05-03 15:26:13 +0200

    Update run_all_RG.m

--- run_all_RG.m
+++ run_all_RG.m
@@ -1,4 +1,4 @@
-% RG_all
+% Main script loading all images and calling the functions to compute features and plot results
 list = dir('*.tif');
 
 results = zeros(length(list)/2,5);
@@ -114,57 +114,3 @@ end
 
 % Plot and save averaged heatmaps
 plot_data('avg_results_features.csv')
-
-
-
-
-
-
-
-
-
-
-
-
-%Rubbish to be removed
-
-%{
-% Compute data per well
-tot_untreated = [];% zeros(192,5);
-tot_siRNAGFP = [];% zeros(96,5);
-tot_siRNALV = [];% zeros(96,5);
-for ll = 1 : 24 : length(results_averaged)
-  
-    % Compute mean untreated
-    untreated = [ results_averaged( ll: ll+10 -1,:) ; results_averaged(ll+23-1:ll+24-1,:)  ] ;
-    
-    % Compute siRNA GFP
-    siRNAGFP = results_averaged(ll+11-1:ll+16-1,:) ;     
-    
-    % Compute siRNA live dead
-    siRNALV = results_averaged(ll+17-1:ll+22-1,:);     
-    
-    % Pack results
-    tot_untreated(end+1:end+12,: )  = untreated;
-    tot_siRNAGFP( end+1:end+6,:)  = siRNAGFP;
-    tot_siRNALV( end+1:end+6,:)  = siRNALV;
-end
-labels =  [ zeros(length(tot_untreated(:,1)), 1); ones(length(tot_siRNAGFP(:,1)), 1); 2*ones(length(tot_siRNALV(:,1)), 1)] ;
-for ff = 1 : 4
-    figure; boxplot([tot_untreated(:,ff) ;  tot_siRNAGFP(:,ff) ;  tot_siRNALV(:,ff)],labels );
-    xticklabels({'Untreated' , 'siRNA-GFP' , 'siRNA-LV'})
-    
-    hold on
-       [n1,~]=size(tot_untreated(:,ff));
-       plot(ones(n1,1),tot_untreated(:,ff),'r*')
-       [n1,~]=size( tot_siRNAGFP(:,ff));
-       plot(2*ones(n1,1), tot_siRNAGFP(:,ff),'r*')
-       [n1,~]=size( tot_siRNALV(:,ff));
-       plot(3*ones(n1,1), tot_siRNALV(:,ff),'r*')
-       
-       
-    hold off
-    
-    
-end
-%}
