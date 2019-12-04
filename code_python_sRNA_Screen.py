f60276c69f5323fa72bb3a31844fc642159eb201
Alessandro Crimi <alecrimi@alice.it>
2019-12-04 18:16:05 +0100

    Update code_python_sRNA_Screen.py

--- code_python_sRNA_Screen.py
+++ code_python_sRNA_Screen.py
@@ -4,9 +4,10 @@ Created on Fri Dec 22 09:55:35 2017
 
 @author: achin
 """
-
 import numpy as np
 import pandas as pd
+
+# Create the plate map according to the indexing for selection by position
 def create_plate_map (Data_Frame, index_start,index_end, temp_repeat, previous_val):
     Data_Frame_temp = Data_Frame[index_start:index_end]
     del Data_Frame_temp['Gene ID']
@@ -31,6 +32,7 @@ def create_plate_map (Data_Frame, index_start,index_end, temp_repeat, previous_v
             gene_id.append(str(initial_val))
     return Data_Frame_temp, library, gene_id, initial_val
 
+# Load data according to plate name
 def get_library (full_library_name):
     #print (full_library_name)
     separator_pos = full_library_name.find('-')
@@ -38,6 +40,14 @@ def get_library (full_library_name):
     library_name = library_name_no[1]
     return library_name
 
+def process_file (ind):    
+    file = "plate_P_" + str(ind)+ ".csv"
+    df = pd.read_csv (file)
+    return df
+frames = [ process_file(i) for i in range(1, no_plates+1)]
+result = pd.concat(frames)
+path = 'plate_all.csv' 
+result.to_csv (path, index = False)  
     
 """ Read the csv sheet to pandas dataframe """
 index_start = 0
@@ -47,7 +57,7 @@ count_A = 0
 count_B = 0
 count_C = 0
 temp_repeat = 0
-Data_Frame = pd.read_csv('C:\\sRNA_screen_MDD\Mouse_screen\\Test\\Silencer Select hGenome List.csv') # E:\\files\\Silencer Select hGenome List.csv
+Data_Frame = pd.read_csv('Silencer Select hGenome List.csv') # E:\\files\\Silencer Select hGenome List.csv
 
 indexes_A = []
 indexes_B = []
@@ -55,6 +65,7 @@ indexes_C = []
 count_all_rows_A = 0
 count_all_rows_B = 0
 count_all_rows_C = 0
+# Load data according to plate name
 for index, row in Data_Frame.iterrows():
      library = get_library(Data_Frame['Plate Name'].iloc[index])
      if library == 'A':
@@ -67,20 +78,22 @@ for index, row in Data_Frame.iterrows():
          indexes_C.append (index)
          count_all_rows_C = count_all_rows_C + 1
 
-
+# Purely integer-location based indexing for selection by position
 data_frame_A = Data_Frame.iloc[indexes_A]
 data_frame_B = Data_Frame.iloc[indexes_B]
 data_frame_C = Data_Frame.iloc[indexes_C]
 
-data_frame_A.to_csv ('C:\\sRNA_screen_MDD\Mouse_screen\\Test\\Library_A.csv', index = False)
-data_frame_B.to_csv ('C:\\sRNA_screen_MDD\Mouse_screen\\Test\\Library_B.csv', index = False)
-data_frame_C.to_csv ('C:\\sRNA_screen_MDD\Mouse_screen\\Test\\Library_C.csv', index = False)
+# Save the indexed data into csv files
+data_frame_A.to_csv ('Library_A.csv', index = False)
+data_frame_B.to_csv ('Library_B.csv', index = False)
+data_frame_C.to_csv ('Library_C.csv', index = False)
 
 count_A = 0
 temp_repeat = 0
 data_frame_A['Plate Name'] = 'Hm Drgbl Gen siRNA Lib-ABC'
 no_plates = 0
  
+# Map source and destination according to plate picklists reported    
 for ind, row in data_frame_A.iterrows():    
     gene = data_frame_A['RefSeq Accession'].iloc[count_A] 
     well =  data_frame_A['Location (Row-Col)'].iloc[count_A] 
@@ -113,7 +126,9 @@ for ind, row in data_frame_A.iterrows():
         Data_Frame_temp.source_plate_id[Data_Frame_temp.source_plate_id != ''] = 'Source_P' + Data_Frame_temp.source_plate_id
         #Data_Frame_temp['source_plate_id']= 'Source_P' + Data_Frame_temp['source_plate_id'] 
         no_plates = no_plates + 1
-        path = 'C:\\sRNA_screen_MDD\Mouse_screen\\Test\\plates\\plate' + '_' + 'P' + '_' + str(no_plates) + '.csv'
+        
+        # Save the mapping
+        path = 'plates\\plate' + '_' + 'P' + '_' + str(no_plates) + '.csv'
         Data_Frame_temp.to_csv (path, index = False)  
         
         temp_repeat = temp_repeat + 1
@@ -121,11 +136,3 @@ for ind, row in data_frame_A.iterrows():
     """ Count index data frame """
     count_A = count_A + 1
     
-def process_file (ind):    
-    file = "C:\\sRNA_screen_MDD\Mouse_screen\\Test\\plate_P_" + str(ind)+ ".csv"
-    df = pd.read_csv (file)
-    return df
-frames = [ process_file(i) for i in range(1, no_plates+1)]
-result = pd.concat(frames)
-path = 'C:\\sRNA_screen_MDD\Mouse_screen\\Test\\plate_all.csv' 
-result.to_csv (path, index = False)  
